49d25a02830efa398d9839921fc0422f01db26b9
LeoCaronezi <rafael.caronezi@outlook.com>
2019-03-07 15:10:49 -0300

    LeoCaronezi
    primeiro exercio apresentando ola mundo

---
+++ Olamundo/src/olaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rafae
+ */
+public class olaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 6;
+        int b =8;
+        
+        a--;
+        b++;
+      System.out.print(++a);
+      System.out.print(b++);    }
+    
+}
