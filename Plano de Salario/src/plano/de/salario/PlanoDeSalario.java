a248be9a9bdf1125c459f3d67399467900ddd895
LeoCaronezi <rafael.caronezi@outlook.com>
2019-03-14 17:31:31 -0300

    LeoCaronezi
    trabalho nao concluido

---
+++ Plano de Salario/src/plano/de/salario/PlanoDeSalario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plano.de.salario;
+
+/**
+ *
+ * @author rafae
+ */
+public class PlanoDeSalario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
