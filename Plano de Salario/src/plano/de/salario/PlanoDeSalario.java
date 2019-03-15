59dbc220211f68f968e51475c924d0da8b1047f5
LeoCaronezi <rafael.caronezi@outlook.com>
2019-03-14 23:49:56 -0300

    LeoCaronezi
    codigo esta com erro descobri e comentar o erro encontrado

--- Plano de Salario/src/plano/de/salario/PlanoDeSalario.java
+++ Plano de Salario/src/plano/de/salario/PlanoDeSalario.java
@@ -13,53 +13,22 @@ public class PlanoDeSalario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      Scanner categoria = new Scanner(System.in);
-      int categ, porcentagem; 
-      double salario, novosalario; 
-      System.out.println("Digite qual categoria  você pertencer A,B,C:");
-      System.out.println("Digite 1-a, 2-b-,3-c:");
-      categ = categoria.nextInt();
-      //continuar depois daqui
+      Scanner  scan = new Scanner (System.in);
+      double salario, novosalario;
+      int cont;
+      System.out.println("escolha qual categoria voce pertence");
+      System.out.println("1- grupo a:");
+      System.out.println("2-grupo b:");
+      System.out.println("3-grupo c:");
+      cont = scan.nextInt();
       System.out.println("Digite seu salario:");
-      salario = categoria.nextDouble();
-      
-      if (categ == 1){
-          porcentagem = 15;
-           novosalario = (porcentagem/100)* salario +(salario);  
-          System.out.println("Seu salario sofreu um aumento de 10 porcento");
-          System.out.println(novosalario);
-      }
-      if (categ == 2){
-           novosalario = ((15/100) * salario ) + salario;
-           System.out.println("Seu aumento foi de 15 porcento " + novosalario);
-         
-      }
-      if (categ == 3) {
-             novosalario = ((20/100) * salario ) + salario;
-             System.out.println ("Seu aumento foi de 20 porcento: "+ novosalario);
-          
-                  
-      }
-      
-      // analisar depois aonde esta desse bloco de codigo.
-      /*switch (categ){
-          case 1:{
-           novosalario = (salario * (10/100)) + salario; 
-          System.out.println("Seu salario sofreu um aumento de 10 porcento");
-          System.out.println(novosalario);
-          break;
+      salario = Double.parseDouble(scan.toString());
+      switch (cont){
+          case 1 :{
+              novosalario = (((10/100) * salario ) + salario);
+              System.out.println(novosalario);
+              break;
           }
-          case 2:{ 
-           novosalario = ((15/100) * salario ) + salario;
-           System.out.println("Seu aumento foi de 15 porcento " + novosalario);
-           break;
       }
-          case 3 : {
-             novosalario = ((20/100) * salario ) + salario;
-            System.out.println ("Seu aumento foi de 20 porcento: "+ novosalario);
-            break;
-          }
-    }
-    */
 }
 }
