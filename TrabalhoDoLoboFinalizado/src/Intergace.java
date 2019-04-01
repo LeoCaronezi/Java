caeea3332fef9e4409086a96e920db2ccdd3c08f
LeoCaronezi <rafael.caronezi@outlook.com>
2019-04-01 01:05:12 -0300

    LeoCaronezi
    atualizando

--- TrabalhoDoLoboFinalizado/src/Intergace.java
+++ TrabalhoDoLoboFinalizado/src/Intergace.java
@@ -16,13 +16,35 @@ public class Intergace extends javax.swing.JFrame {
     /**
      * Creates new form Intergace
      */
- 
+    int contador1; 
+    int contador2;
     String jogada;
     int jogo =1;
-    
+    int vit;
     public Intergace() {
         initComponents();
     }
+    public void vitoria(){
+   //     vit = jogo;
+        btn1.setEnabled(false);
+        btn2.setEnabled(false);
+        btn3.setEnabled(false);
+        btn4.setEnabled(false);
+        btn5.setEnabled(false);
+        btn6.setEnabled(false);
+        btn7.setEnabled(false);
+        btn8.setEnabled(false);
+        btn9.setEnabled(false);
+    }
+    
+  /*  public void  contadorVitoria(){
+        if (vitoria() == 1){
+            contador1++;
+        }
+        else {
+            contador2++;
+        }
+    }*/
    public void jogada (JButton v){
        if (jogo == 1){
            jogada ="X";
@@ -41,39 +63,70 @@ public class Intergace extends javax.swing.JFrame {
    public void verifica (){
        // verificando a primeira linha x: 
        if(btn1.getText().contains("X") && btn2.getText().contains("X") && btn3.getText().contains("X")){
-       
+          btn1.setEnabled(false);
+        btn2.setEnabled(false);
+        btn3.setEnabled(false);
+        btn4.setEnabled(false);
+        btn5.setEnabled(false);
+        btn6.setEnabled(false);
+        btn7.setEnabled(false);
+        btn8.setEnabled(false);
+        btn9.setEnabled(false);       
+//vitoria();
    } 
        //verifica a segunda linha:
        if(btn4.getText().contains("X") && btn5.getText().contains("X") && btn6.getText().contains("X"))
        {
-           
+           vitoria();
        }
        // verifica a terceira linha  x:
        if(btn7.getText().contains("X") && btn8.getText().contains("X") && btn9.getText().contains("X")) {
-           
+           vitoria();
        }
        // verifica a primeira coluna x:
        if(btn1.getText().contains("X") && btn4.getText().contains("X") && btn7.getText().contains("X")){
-           
+           vitoria();
        }
        
        //verifica a  segunda coluna x:
        if(btn2.getText().contains("X") && btn5.getText().contains("X") && btn8.getText().contains("X")){
-           
+           vitoria();
        }
        // verifica a terceira x:
            if(btn3.getText().contains("X") && btn6.getText().contains("X") && btn9.getText().contains("X")){
-               
+               vitoria();
            }
            
            // vericiando diagonal 1 :
            if(btn1.getText().contains("X") && btn5.getText().contains("X") && btn9.getText().contains("X")){
-               
+               vitoria();
            }
            //verifica segunda diagonal 2  x:
            if(btn3.getText().contains("X") && btn5.getText().contains("X") && btn9.getText().contains("X")){
-               
+               vitoria();
            }
+           
+           //verificando a primeira linha 1 0 ^:
+           if(btn1.getText().contains("O") && btn2.getText().contains("O") && btn3.getText().contains("O")){
+               vitoria();
+           }
+           //verificando a segunda linha 0:
+           if(btn4.getText().contains("O") && btn5.getText().contains("O") && btn6.getText().contains("O")){
+              // vitoria();
+           }
+           //verificando a terceira linha 0:
+           if(btn7.getText().contains("O") && btn8.getText().contains("O") && btn9.getText().contains("O")){
+               vitoria();
+           }
+           // verificando a diagonal 
+           if(btn3.getText().contains("O") && btn5.getText().contains("O") && btn7.getText().contains("O")){
+               vitoria();
+           }
+           //verificando a diagonal
+           if(btn1.getText().contains("O") && btn5.getText().contains("O") && btn9.getText().contains("O")){
+               vitoria();
+           }
+           
    }
    
    
@@ -101,6 +154,9 @@ public class Intergace extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         turno = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        contadordevitoriaJogador1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -228,15 +284,34 @@ public class Intergace extends javax.swing.JFrame {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Informações Do Jogo:", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Times New Roman", 0, 18))); // NOI18N
 
+        jLabel2.setText("Jogador1:");
+
+        jLabel3.setText("vitorias:");
+
+        contadordevitoriaJogador1.setText("XXXX");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 289, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(contadordevitoriaJogador1)
+                .addContainerGap(143, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 240, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(contadordevitoriaJogador1))
+                .addContainerGap(215, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -346,7 +421,10 @@ public class Intergace extends javax.swing.JFrame {
     private javax.swing.JButton btn7;
     private javax.swing.JButton btn8;
     private javax.swing.JButton btn9;
+    private javax.swing.JLabel contadordevitoriaJogador1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel turno;
