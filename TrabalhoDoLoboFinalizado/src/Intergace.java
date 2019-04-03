b9c6acdd311f0848aaf96d89e7427ac6fed96219
LeoCaronezi <rafael.caronezi@outlook.com>
2019-04-03 15:45:52 -0300

    LeoCaronezi
    atualização de componetes

--- TrabalhoDoLoboFinalizado/src/Intergace.java
+++ TrabalhoDoLoboFinalizado/src/Intergace.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import javax.swing.JButton;
 
 /**
@@ -6,10 +7,6 @@ import javax.swing.JButton;
  * @author rafael caronezi
  */
 public class Intergace extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Intergace
-     */
     int contador1; 
     int contador2;
     String jogada,winner;
@@ -27,33 +24,37 @@ public class Intergace extends javax.swing.JFrame {
            jogada ="X";
             jogo = 0;  
             turno.setText("Jogador 1. ");
-            winner = "Jogador 1";
+
             v.setEnabled(false);
-            verifica();
+            
        }
        else {
            jogada ="O";
            jogo = 1;
            turno.setText("Jogador 2.");
            v.setEnabled(false);
-           winner ="Jogador 2";
-           verifica();
+                  
        }
        v.setText(jogada);
+       verifica();
    }
    public void verifica (){
        // verificando a primeira linha x: 
        if(btn1.getText().contains("X") && btn2.getText().contains("X") && btn3.getText().contains("X")){
           btn1.setEnabled(false);
-        btn2.setEnabled(false);
-        btn3.setEnabled(false);
-        btn4.setEnabled(false);
-        btn5.setEnabled(false);
-        btn6.setEnabled(false);
-        btn7.setEnabled(false);
-        btn8.setEnabled(false);
-        btn9.setEnabled(false);
-        vecendor.setText(winner);
+          btn2.setEnabled(false);
+          btn3.setEnabled(false);
+          btn4.setEnabled(false);
+          btn5.setEnabled(false);
+          btn6.setEnabled(false);
+          btn7.setEnabled(false);
+          btn8.setEnabled(false);
+          btn9.setEnabled(false);
+          vecendor.setText("Jogador 1:");
+          btn1.setForeground(Color.red);
+          btn2.setForeground(Color.red);
+          btn3.setForeground(Color.red);
+          
 //vitoria();
    } 
        //verifica a segunda linha:
@@ -67,7 +68,10 @@ public class Intergace extends javax.swing.JFrame {
         btn7.setEnabled(false);
         btn8.setEnabled(false);
         btn9.setEnabled(false);
-        vecendor.setText(winner);
+        vecendor.setText("Jogoador 1");
+        btn4.setForeground(Color.red);
+        btn5.setForeground(Color.red);
+        btn6.setForeground(Color.red);
        }
        // verifica a terceira linha  x:
        if(btn7.getText().contains("X") && btn8.getText().contains("X") && btn9.getText().contains("X")) {
@@ -80,7 +84,10 @@ public class Intergace extends javax.swing.JFrame {
         btn7.setEnabled(false);
         btn8.setEnabled(false);
         btn9.setEnabled(false);
-        vecendor.setText(winner);
+        vecendor.setText("Jogador 1");
+        btn7.setForeground(Color.red);
+        btn8.setForeground(Color.red);
+        btn9.setForeground(Color.red);
        }
        // verifica a primeira coluna x:
        if(btn1.getText().contains("X") && btn4.getText().contains("X") && btn7.getText().contains("X")){
@@ -93,21 +100,27 @@ public class Intergace extends javax.swing.JFrame {
         btn7.setEnabled(false);
         btn8.setEnabled(false);
         btn9.setEnabled(false);
-        vecendor.setText(winner);
+        vecendor.setText("Jogador 1");
+        btn1.setForeground(Color.red);
+        btn4.setForeground(Color.red);
+        btn7.setForeground(Color.red);
        }
        
        //verifica a  segunda coluna x:
        if(btn2.getText().contains("X") && btn5.getText().contains("X") && btn8.getText().contains("X")){
            btn1.setEnabled(false);
-        btn2.setEnabled(false);
-        btn3.setEnabled(false);
-        btn4.setEnabled(false);
-        btn5.setEnabled(false);
-        btn6.setEnabled(false);
-        btn7.setEnabled(false);
-        btn8.setEnabled(false);
-        btn9.setEnabled(false);
-        vecendor.setText(winner);
+           btn2.setEnabled(false);
+           btn3.setEnabled(false);
+           btn4.setEnabled(false);
+           btn5.setEnabled(false);
+           btn6.setEnabled(false);
+           btn7.setEnabled(false);
+           btn8.setEnabled(false);
+           btn9.setEnabled(false);
+          vecendor.setText("Jogador 1");
+          btn2.setForeground(Color.red);
+          btn5.setForeground(Color.red);
+          btn8.setForeground(Color.red);
        }
        // verifica a terceira x:
            if(btn3.getText().contains("X") && btn6.getText().contains("X") && btn9.getText().contains("X")){
@@ -120,7 +133,10 @@ public class Intergace extends javax.swing.JFrame {
         btn7.setEnabled(false);
         btn8.setEnabled(false);
         btn9.setEnabled(false);
-        vecendor.setText(winner);
+        vecendor.setText("jogador 1");
+        btn3.setForeground(Color.red);
+        btn6.setForeground(Color.red);
+        btn9.setForeground(Color.red);
            }
            
            // vericiando diagonal 1 :
@@ -134,20 +150,26 @@ public class Intergace extends javax.swing.JFrame {
         btn7.setEnabled(false);
         btn8.setEnabled(false);
         btn9.setEnabled(false);
-        vecendor.setText(winner);
+        vecendor.setText("Jogador 1");
+        btn1.setForeground(Color.red);
+        btn5.setForeground(Color.red);
+        btn9.setForeground(Color.red);
            }
            //verifica segunda diagonal 2  x:
            if(btn3.getText().contains("X") && btn5.getText().contains("X") && btn9.getText().contains("X")){
               btn1.setEnabled(false);
-        btn2.setEnabled(false);
-        btn3.setEnabled(false);
-        btn4.setEnabled(false);
-        btn5.setEnabled(false);
-        btn6.setEnabled(false);
-        btn7.setEnabled(false);
-        btn8.setEnabled(false);
-        btn9.setEnabled(false);
-        vecendor.setText(winner);
+              btn2.setEnabled(false);
+              btn3.setEnabled(false);
+              btn4.setEnabled(false);
+              btn5.setEnabled(false);
+              btn6.setEnabled(false);
+              btn7.setEnabled(false);
+              btn8.setEnabled(false);
+              btn9.setEnabled(false);
+              vecendor.setText("jOGADOR 1");
+              btn3.setForeground(Color.red);
+              btn5.setForeground(Color.red);
+              btn9.setForeground(Color.red);
            }
            
            //verificando a primeira linha 1 0 ^:
@@ -161,7 +183,10 @@ public class Intergace extends javax.swing.JFrame {
              btn7.setEnabled(false);
              btn8.setEnabled(false);
              btn9.setEnabled(false);
-             vecendor.setText(winner);
+             vecendor.setText("jOGADOR 2");
+             btn1.setForeground(Color.blue);
+             btn2.setForeground(Color.blue);
+             btn3.setForeground(Color.blue);
            }
            //verificando a segunda linha 0:
            if(btn4.getText().contains("O") && btn5.getText().contains("O") && btn6.getText().contains("O")){
@@ -174,20 +199,26 @@ public class Intergace extends javax.swing.JFrame {
              btn7.setEnabled(false);
              btn8.setEnabled(false);
             btn9.setEnabled(false);
-            vecendor.setText(winner);
+            vecendor.setText("JOGADOR 2 ");
+               btn4.setForeground(Color.blue);
+               btn5.setForeground(Color.blue);
+               btn6.setForeground(Color.blue);
            }
            //verificando a terceira linha 0:
            if(btn7.getText().contains("O") && btn8.getText().contains("O") && btn9.getText().contains("O")){
                 btn1.setEnabled(false);
-             btn2.setEnabled(false);
-             btn3.setEnabled(false);
-             btn4.setEnabled(false);
-             btn5.setEnabled(false);
-             btn6.setEnabled(false);
-             btn7.setEnabled(false);
-             btn8.setEnabled(false);
-             btn9.setEnabled(false);
-             vecendor.setText(winner);
+                btn2.setEnabled(false);
+                btn3.setEnabled(false);
+                btn4.setEnabled(false);
+                btn5.setEnabled(false);
+                btn6.setEnabled(false);
+                btn7.setEnabled(false);
+                btn8.setEnabled(false);
+                btn9.setEnabled(false);
+             vecendor.setText("jOGADOR 2");
+                btn7.setForeground(Color.blue);
+                btn8.setForeground(Color.blue);
+                btn9.setForeground(Color.blue);
            }
            // verificando a diagonal 
            if(btn3.getText().contains("O") && btn5.getText().contains("O") && btn7.getText().contains("O")){
@@ -200,6 +231,10 @@ public class Intergace extends javax.swing.JFrame {
                btn7.setEnabled(false);
                btn8.setEnabled(false);
                btn9.setEnabled(false);
+               vecendor.setText("jOGADOR 2");
+                btn7.setForeground(Color.blue);
+                btn5.setForeground(Color.blue);
+                btn3.setForeground(Color.blue);
            }
            //verificando a diagonal
            if(btn1.getText().contains("O") && btn5.getText().contains("O") && btn9.getText().contains("O")){
@@ -212,20 +247,15 @@ public class Intergace extends javax.swing.JFrame {
               btn7.setEnabled(false);
               btn8.setEnabled(false);
               btn9.setEnabled(false);
-              vecendor.setText(winner);
+              vecendor.setText("jOGADOR 2");
+             btn5.setForeground(Color.blue);
+             btn9.setForeground(Color.blue);
+             btn1.setForeground(Color.blue);
            }
            
    }
    
-   
-   
-   
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
-    @SuppressWarnings("unchecked")
+     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -434,38 +464,47 @@ public class Intergace extends javax.swing.JFrame {
 
     private void btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn1ActionPerformed
       jogada(btn1);
+      verifica ();
     }//GEN-LAST:event_btn1ActionPerformed
 
     private void btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn2ActionPerformed
      jogada(btn2);
+     verifica ();
     }//GEN-LAST:event_btn2ActionPerformed
 
     private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
       jogada(btn3);
+      verifica ();
     }//GEN-LAST:event_btn3ActionPerformed
 
     private void btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn4ActionPerformed
      jogada(btn4);
+     verifica ();
     }//GEN-LAST:event_btn4ActionPerformed
 
     private void btn5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn5ActionPerformed
      jogada (btn5);
+     verifica ();
     }//GEN-LAST:event_btn5ActionPerformed
 
     private void btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn6ActionPerformed
       jogada (btn6);
+      verifica ();
     }//GEN-LAST:event_btn6ActionPerformed
 
     private void btn7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn7ActionPerformed
        jogada (btn7);
+       verifica ();
     }//GEN-LAST:event_btn7ActionPerformed
 
     private void btn8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn8ActionPerformed
        jogada (btn8);
+       verifica ();
     }//GEN-LAST:event_btn8ActionPerformed
 
     private void btn9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn9ActionPerformed
         jogada (btn9);
+        verifica ();
     }//GEN-LAST:event_btn9ActionPerformed
 
     /**
