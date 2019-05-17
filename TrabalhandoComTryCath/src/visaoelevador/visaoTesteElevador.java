c9f8b1df35e97fa00865fbfc72941e05bfc06e0b
LeoCaronezi <rafael.caronezi@outlook.com>
2019-05-17 14:18:42 -0300

    LeoCaronezi
    removendo o case e colocando o if

--- TrabalhandoComTryCath/src/visaoelevador/visaoTesteElevador.java
+++ TrabalhandoComTryCath/src/visaoelevador/visaoTesteElevador.java
@@ -6,9 +6,10 @@
 package visaoelevador;
 import javax.swing.JOptionPane;
 import  modeloelevador.Elevador;
+import java.util.Scanner;
 /**
  *
- * @author rafae
+ * @author rafael caronezi11
  */
 public class visaoTesteElevador {
 
@@ -16,28 +17,54 @@ public class visaoTesteElevador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       Scanner  leitor = new Scanner (System.in);
        Elevador elevador = new Elevador();
        elevador.IniciarElevador();
-       elevador.op =Integer.parseInt(JOptionPane.showInputDialog(null, args,"Escolha 1- para entra, "
-               + "2- Para subir, 3- Para descer, 4- Sair, 5- sair"));
-       while(elevador.op != 6){
-       switch (elevador.op){
-           case 1: 
+       System.out.println("informe a qauntidade de andares:");
+       elevador.andar= Integer.parseInt(leitor.nextLine());
+       System.out.println("Escolha 1- para entra, 2- para subir,3- para sair "
+               + "4- para descer, 5- para sair");
+       
+       elevador.op=Integer.parseInt(leitor.nextLine());
+       
+       //corrigir caixa de texto
+     /*  elevador.op =Integer.parseInt(JOptionPane.showInputDialog(null, args,"Escolha 1- para entra, "
+               + "2- Para subir, 3- Para descer, 4- Sair, 5- sair")); */
+       
+     
+           //entra no elevador:
+           if(elevador.op==1) {
+               System.out.println("informe pessoas a entra no elevador:");
+               elevador.pessoasAentrar = Integer.parseInt(leitor.nextLine());
                elevador.EntraElevador();
-               break;
-           case 2: 
+                
+           }
+               
+            //subir elevador: 
+           if (elevador.op==2) { 
+               System.out.println("Informe o andar desejado:");
+               elevador.andarDesejado= Integer.parseInt(leitor.nextLine());
                elevador.SubirElevador();
                
-               break;
-           case 3:
+               }
+           // sair do elevador 
+           if (elevador.op==3){
+               System.out.println("Informe o numero de pesosas que iram sair do "
+                       + "elevador");
+               elevador.pessoasAsair= Integer.parseInt(leitor.nextLine());
                elevador.SairElevador();
-               break;
-           case 4:
+               }
+           //descer elevador
+            if(elevador.op==4){
+               System.out.println("Informe o andar desejado:");
+               elevador.andarDesejado= Integer.parseInt(leitor.nextLine());
+               elevador.SubirElevador();
                elevador.DescerElevador();
-               break;
-           case 5:
+               }
+            // finalizar programa     
+           if(elevador.op==5){
                 elevador.FinalizarElevador();
        }
     }
     }
-}
+
